9b94130e40f72fee0342bf75ab0effa2a0f23c66
user <>
2019-07-23 21:33:15 -0700

    + lab4 complete

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static('public'));
+
+// routes
+app.route('/').get((req, resp) => resp.render('index.html', {nav: req.url}));
+app.get('/mercury', (req, resp) => resp.render('mercury.html', {nav: req.url}));
+app.get('/venus', (req, resp) => resp.render('venus.html', {nav: req.url}));
+app.get('/earth', (req, resp) => resp.render('earth.html', {nav: req.url}));
+app.get('/mars', (req, resp) => resp.render('mars.html', {nav: req.url}));
+// app.get('/venus', (req, resp) => resp.send('Venus Page'));
+
+
+// server listener
+app.listen('8081', '0.0.0.0', () => console.log('express server is running...'));
---
+++ readme.md
@@ -0,0 +1,31 @@
+App reqs:
++ ✓ bootstrap on all pages
++ ✓ external css or bootstrap
++ ✓ 5+ routes
++ ✓ 1+ image & content for:
+  - ✓ Mercury
+  - ✓ Venus
+  - ✓ Earth
+  - added Mars for #5
++ ✓ Nav for all pages
++ ✓ uses head & footer partials
++ passes w3 validator
+
+
+https://scotch.io/tutorials/use-ejs-to-template-your-node-application
+
+solar system
+https://picryl.com/media/arc-1972-ac72-1279-70b4b8
+
+venus
+https://picryl.com/media/arc-1978-ac78-0238-064647
+
+mercury
+https://www.gutenberg.org/ebooks/50885
+
+earth
+https://picryl.com/media/an-artists-concept-of-the-nato-defense-satellite-communications-system-ii-satellite-7b0bd3
+
+
+mars
+https://picryl.com/media/mars-sun-solar-system-science-technology-8fd79e
